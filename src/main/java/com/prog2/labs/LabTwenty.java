d285a5ad7bcf6ab199262de09f70f874332a1d09
Adin Ashby <adin.ashby@gmail.com>
2022-11-17 07:55:07 -0500

    first commit

---
+++ src/main/java/com/prog2/labs/LabTwenty.java
@@ -0,0 +1,23 @@
+package com.prog2.labs;
+
+/**
+ * @author adinashby
+ *
+ */
+
+public class LabTwenty {
+
+	/**
+	 * Write your test code below in the main (optional).
+	 *
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * Please refer to the README file for question(s) description
+	 */
+
+
+}
